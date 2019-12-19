ab2f84c412a99c9b4197f51fcd0ad366abc25b5c
necabo <fynn.becker@gmx.de>
2019-12-19 21:39:07 +0100

    Add leaderboard exercise

---
+++ chapter6/leaderboard.cpp
@@ -0,0 +1,42 @@
+#include "../std_lib_facilities.h"
+
+class Name_value {
+    public:
+        string name;
+        int value;
+};
+
+bool contains(vector<Name_value> scores, string name) {
+    for (size_t i = 0; i < scores.size(); ++i) {
+        if (scores[i].name == name) return true;
+    }
+    return false;
+}
+
+void print_scores(vector<Name_value> scores) {
+    for (size_t i = 0; i < scores.size(); ++i) {
+        cout << scores[i].name << ' ' << scores[i].value << '\n';
+    }
+}
+
+int main() {
+    try {
+        vector<Name_value> scores;
+        string name;
+        int value;
+        while (cin >> name >> value && !(name == "NoName" && value == 0)) {
+            cout << name << '\n';
+            if (contains(scores, name)) simple_error("duplicate name");
+            scores.push_back(Name_value{name, value});
+        }
+        print_scores(scores);
+
+    } catch (exception& e) {
+        cerr << "Error: " << e.what() << '\n';
+        return 1;
+    } catch (...) {
+        cerr << "Unknown exception!\n";
+        return 2;
+    }
+    return 0;
+}
